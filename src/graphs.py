840050d615f2220a9776f69a5d6b025314f27de6
AlexandreDoneux <94830560+AlexandreDoneux@users.noreply.github.com>
2025-11-26 23:16:00 +0100

    adding python graph script

---
+++ src/graphs.py
@@ -0,0 +1,40 @@
+# Graph generation script
+#
+# Uses csv files to generate graphs for the different experiments
+# - philosophers.csv
+# - producers_consumers.csv
+# - writers_readers.csv
+# - atomic_exclusion_algo.csv ("test-and-set", "test-and-test-and-set")
+
+# Those graphs represent the computation or access speed for different number of threads. Each one contains mean values ad standart deviation.
+# mean = sum off all values divided by the number of values
+# standart deviation = *voir formule en ligne*
+
+# Python script used to generate the graphs from the csv files.
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def plot_thread_results(csv_path, graph_name):
+    data_frame = pd.read_csv(csv_path)
+    threads = data_frame.columns[1:]
+
+    averages = data_frame[threads].mean()
+    standard_deviations = data_frame[threads].std()
+
+    # Plot
+    plt.figure(figsize=(10, 6))
+    plt.bar(threads, averages, yerr=standard_deviations, ecolor="red", capsize=5)
+
+    plt.title(graph_name)
+    plt.xlabel("Number of Threads")
+    plt.ylabel("Execution Time")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
+
+
+plot_thread_results("phil.csv", graph_name="Dining Philosophers Execution Time")
+
+# adding saving as png
