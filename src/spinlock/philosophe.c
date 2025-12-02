7f888923521e7637f57252884b6a84e86b59d911
AlexandreDoneux <94830560+AlexandreDoneux@users.noreply.github.com>
2025-12-03 00:32:29 +0100

    fix typo in philosophe file name

---
+++ src/spinlock/philosophe.c
@@ -0,0 +1,102 @@
+// Philosopher simulation using spinlocks
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#include "mysem.h"
+
+#define CYCLES 1000000
+
+struct args {
+    int id;
+    int philosophes;
+    mysem_t *baguette;   // au lieu de thread_mutex_t *baguette;
+};
+
+void mange(int id)
+{
+    for (int i = 0; i < rand(); i++)
+    {
+        // philosophe mange
+    }
+}
+
+void *philosophe(void *args)
+{
+    int id = ((struct args*)args)->id;
+    int left = id;
+    int philosophes = ((struct args*)args)->philosophes;
+    mysem_t *baguette = ((struct args*)args)->baguette;
+    int right = (left + 1) % philosophes;
+    int count = 0;
+
+    while (count < CYCLES)
+    {
+        // philosophe pense
+        //printf("Philosophe [%d] thinking\n", id);
+
+        if (left < right)
+        {
+            mysem_wait(&baguette[left]);
+            //printf("baguette left taken Philosophe [%d]\n", id);
+            mysem_wait(&baguette[right]);
+            //printf("baguette right taken Philosophe [%d]\n", id);
+        }
+        else
+        {
+            mysem_wait(&baguette[right]);
+            //printf("baguette right taken Philosophe [%d]\n", id);
+            mysem_wait(&baguette[left]);
+            //printf("baguette left taken Philosophe [%d]\n", id);
+        }
+
+        mange(id);
+
+        mysem_post(&baguette[left]);
+        //printf("baguette left free Philosophe [%d]\n", id);
+        mysem_post(&baguette[right]);
+        //printf("baguette right free Philosophe [%d]\n", id);
+
+        count++;
+    }
+    return NULL;
+}
+
+int main(void)
+{
+    int philosophes;
+    printf("number of philosophers: \n");
+    if (scanf("%d", &philosophes) != 1 || philosophes <= 0) {
+        fprintf(stderr, "Invalid number of philosophers\n");
+        return EXIT_FAILURE;
+    }
+
+    pthread_t *phil = malloc(philosophes * sizeof(pthread_t));
+    mysem_t *baguette = malloc(philosophes * sizeof(mysem_t));
+    struct args *PhilArgs = malloc(philosophes * sizeof(struct args));
+
+    if (!phil || !baguette || !PhilArgs) {
+        fprintf(stderr, "malloc error\n");
+        return EXIT_FAILURE;
+    }
+
+    for (int i = 0; i < philosophes; i++) {
+        mysem_init(&baguette[i], 1);  
+    }
+
+    for (int i = 0; i < philosophes; i++) {
+        PhilArgs[i].philosophes = philosophes;
+        PhilArgs[i].baguette = baguette;
+        PhilArgs[i].id = i;
+        pthread_create(&phil[i], NULL, philosophe, &PhilArgs[i]);
+    }
+
+    for (int i = 0; i < philosophes; i++) {
+        pthread_join(phil[i], NULL);
+    }
+
+    return 0;
+}
